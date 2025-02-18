7ccab9442c15e809684b34391fd47b90e808c8db
Marcio Caldeira <162082815+marcio-caldeira@users.noreply.github.com>
2025-02-18 00:59:11 -0300

    Update engine.js
    Atualização contagem de lives, add reload e add confirm para o usuário decidir se quer recarregar o jogo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,12 +4,14 @@ const state = {
     enemy: document.querySelector(".enemy"),
     timeLeft: document.querySelector("#time-left"),
     score: document.querySelector("#score"),
+    lives: document.querySelector("#lives"), //Exibir vidas
   },
   values: {
     gameVelocity: 1000,
     hitPosition: 0,
     result: 0,
     curretTime: 60,
+    lives: 3, // adicionando vidas
   },
   actions: {
     timerId: setInterval(randomSquare, 1000),
@@ -25,6 +27,9 @@ function countDown() {
     clearInterval(state.actions.countDownTimerId);
     clearInterval(state.actions.timerId);
     alert("Game Over! O seu resultado foi: " + state.values.result);
+    if (confirm("Deseja jogar novamente?")) {
+      location.reload(); // Recarrega se o jogador confirmar
+    }
   }
 }
 
@@ -48,12 +53,28 @@ function randomSquare() {
 function addListenerHitBox() {
   state.view.squares.forEach((square) => {
     square.addEventListener("mousedown", () => {
-      if (square.id === state.values.hitPosition) {
+      if (square.id === state.values.hitPosition) { //acertou o inimigo
         state.values.result++;
         state.view.score.textContent = state.values.result;
         state.values.hitPosition = null;
         playSound("hit");
+
+      } else {
+        // Errou o inimigo -> Perde uma vida
+        state.values.lives--;
+        state.view.lives.textContent = state.values.lives; // Atualiza no HTML
+
+        if (state.values.lives <= 0) {
+          clearInterval(state.actions.timerId);
+          clearInterval(state.actions.countDownTimerId);
+          
+          alert("Game Over! O seu resultado foi: " + state.values.result);
+          if (confirm("Deseja jogar novamente?")) {
+            location.reload(); // Recarrega se o jogador confirmar
+          }
+        }
       }
+
     });
   });
 }
